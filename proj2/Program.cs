88546bffd18c7ed946795abc23502d434682b3d4
Rebaad <33205237+ReBaad@users.noreply.github.com>
2020-11-03 10:04:10 +0100

    does this work

--- proj2/Program.cs
+++ proj2/Program.cs
@@ -7,6 +7,7 @@ namespace proj2
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            onsole.WriteLine("Hdoes this work!?ld!");
         }
     }
 }
