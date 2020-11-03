bd88b9f26d07b16cc96f761822f36a18e2bba8ef
Alex Østerby Jensen <Alexoej@hotmail.com>
2020-11-03 10:04:22 +0100

    working?

--- proj2/Program.cs
+++ proj2/Program.cs
@@ -7,6 +7,9 @@ namespace proj2
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine("Am i Gitting it now?");
         }
     }
 }
